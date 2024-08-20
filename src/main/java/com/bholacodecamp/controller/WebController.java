b5e071e93ea7065c8167dd52cf09944441e3ca28
anilkumarbaral <anilbaral06@gmail.ocm>
2024-08-20 13:01:30 +0530

    added getUserById features

--- src/main/java/com/bholacodecamp/controller/WebController.java
+++ src/main/java/com/bholacodecamp/controller/WebController.java
@@ -1,14 +1,17 @@
 package com.bholacodecamp.controller;
 
-import com.bholacodecamp.entity.User;
-import com.bholacodecamp.service.*;
-
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.stereotype.*;
-import org.springframework.ui.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.bholacodecamp.entity.User;
+import com.bholacodecamp.service.UserService;
+
+
 
 @Controller
 public class WebController {
@@ -20,6 +23,12 @@ public class WebController {
     	List<User> users = userService.getAllUsers();
         model.addAttribute("users", users);
         return "users";
+    }
+    @GetMapping("/users/{id}")
+    public String getUserById(Model model,@PathVariable("id")Long id) {
+    	User user=userService.getUserById(id);
+    	model.addAttribute("users",user);
+    	return "users";
     }
 }
 
